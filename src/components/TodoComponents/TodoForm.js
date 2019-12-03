5bd5021b4f01ba093967b61b5d32da9fcb0547bd
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2019-12-03 11:40:41 -0800

    mvp flex completed

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function todoForm(props) {
+    return (
+        <form className='todoForm' onSubmit={props.addTodo}>
+            <input  
+                name='task'
+                value={props.task}
+                onChange={props.inputChange}
+                placeholder='Task...'
+            />
+            <button className='buttonAdd' type='submit'>Add Todo</button>
+            <button classname='buttonclr' onClick={props.clear}>Clear</button>
+        </form>
+    )
+}
+
+export default todoForm;
